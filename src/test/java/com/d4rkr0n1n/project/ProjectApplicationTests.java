23561e67dab1268d41788d44a2cdcde4dfa7068d
Mridul Roy <midlry.mr@gmail.com>
2024-07-26 20:09:09 +0530

    chore: Add initial project files and configurations

---
+++ src/test/java/com/d4rkr0n1n/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/io/d4rkr0n1n/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.d4rkr0n1n.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
